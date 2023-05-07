cdb8b6dc44be90557b33e89c88cf293f4350d2b6
Mike Pry <mikepry@hotmail.com>
2023-05-07 10:31:52 -0400

    creating a Holy Bible API

---
+++ main.js
@@ -0,0 +1,8 @@
+const verse = document
+  .getElementById("new-verse-btn")
+  .addEventListener("click", () => {
+    alert("hello VERSE");
+  });
+const fb = document.getElementById("fb-btn").addEventListener("click", () => {
+  alert("hello fb");
+});
