78a1e1c4e244d10ddca771609d4972b279660747
Mike Pry <mikepry@hotmail.com>
2023-05-14 11:19:00 -0400

    trying new font sizes

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ const verse = document
 const fb = document.getElementById("fb-btn").addEventListener("click", () => {
   alert("hello fb");
 });
+const apiKey = "46bb0297b1931afc851a3877bf2f1e50";
+
+const apiUrl = `
+https://api.scripture.api.bible/v1/bibles/kjv/books?include-chapters=true&client_id=${apiKey}`;
+
+async function getVerses() {
+  try {
+    const response = await fetch(apiUrl);
+    versesArray = await response.json();
+  } catch (error) {}
+}
