7f4ca4e0d4c038e48e693622758062ced203e7f0
Mike Pry <mikepry@hotmail.com>
2023-12-24 11:58:24 -0500

    added some Spanish

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
 const verse = document
-  .getElementById("new-verse-btn")
-  .addEventListener("click", () => {
-    alert("hello VERSE");
-  });
-const fb = document.getElementById("fb-btn").addEventListener("click", () => {
-  alert("hello fb");
-});
-
-async function logJSONData() {
-  const response = await fetch("https://bible-api.com");
-  const data = await response.json;
-  console.log(data);
-  // console.log(data);
-}
-// console.log("hello");
-logJSONData();
+  .getElementById("Spanish")
+  .addEventListener("click", () => {});
