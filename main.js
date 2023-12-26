d980b4265c976ae9cb59372109a36e7bfbdb02de
Mike Pry <mikepry@hotmail.com>
2023-12-25 21:47:22 -0500

    added animation to hamburger

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const verse = document
-  .getElementById("Spanish")
-  .addEventListener("click", () => {});
+const hamburger = document.querySelector(".hamburger");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+
+hamburger.addEventListener("click", () => {
+  bar1.classList.toggle("animateBar1");
+  bar2.classList.toggle("animateBar2");
+  bar3.classList.toggle("animateBar3");
+});
