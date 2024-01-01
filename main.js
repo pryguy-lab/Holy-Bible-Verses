636012ad38681d7126f7bf8dea6f80b70f691427
Mike Pry <mikepry@hotmail.com>
2023-12-31 22:36:40 -0500

    added a new animation to the mobile nav

--- main.js
+++ main.js
@@ -3,10 +3,18 @@ const bar1 = document.querySelector(".bar1");
 const bar2 = document.querySelector(".bar2");
 const bar3 = document.querySelector(".bar3");
 const mobileNav = document.querySelector(".mobileNav");
+const closeDrawer = document.querySelector(".closeDrawer");
 
 hamburger.addEventListener("click", () => {
-  bar1.classList.toggle("animateBar1");
-  bar2.classList.toggle("animateBar2");
-  bar3.classList.toggle("animateBar3");
-  mobileNav.classList.toggle("openDrawer");
+  bar1.classList.add("animateBar1");
+  bar2.classList.add("animateBar2");
+  bar3.classList.add("animateBar3");
+  mobileNav.classList.add("openDrawer");
+
+  closeDrawer.addEventListener("click", () => {
+    bar1.classList.remove("animateBar1");
+    bar2.classList.remove("animateBar2");
+    bar3.classList.remove("animateBar3");
+    mobileNav.classList.remove("openDrawer");
+  });
 });
