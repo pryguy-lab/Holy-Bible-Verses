d0e9be75d26158712173f943a88b5952b67c047f
Mike Pry <mikepry@hotmail.com>
2024-02-26 07:30:16 -0500

    adding to the signin page

--- signin.js
+++ signin.js
@@ -1,8 +1,10 @@
 let mainBtn = document.querySelector(".mainBtn");
 let answer = document.querySelector(".answer");
+let midsectionon = document.querySelector(".midsectionon");
 
 mainBtn.addEventListener("click", (e) => {
   e.target.remove();
-  let user = prompt("Please enter your name");
-  answer.innerText = `hello ${user}, welcome back !`;
+  let user = prompt("Please enter your name or press enter");
+  midsectionon.classList.add();
+  answer.innerText = `hello ${user}, welcome to our website!  Please make a selection`;
 });
